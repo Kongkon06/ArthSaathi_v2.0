b782db46038454f4048231c59e6b70ed8618ea92
Abhinov Dutta <abhinavdutta927@gmail.com>
2025-06-11 01:47:55 +0530

    commit done

--- Arth_sangi/app/_layout.tsx
+++ Arth_sangi/app/_layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <RecoilRoot>
       <Stack>
+         <Stack.Screen name="launchscreen" options={{ headerShown: false }} />
         <Stack.Screen name="auth" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
